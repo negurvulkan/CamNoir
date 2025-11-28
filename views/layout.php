8f1939e1a4e47b2c07372854776d95d6f7e5984f
negurvulkan <61453878+negurvulkan@users.noreply.github.com>
2025-11-28 18:32:10 +0100

    Remove PWA functionality

--- views/layout.php
+++ views/layout.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= sanitize_text($title ?? 'NRW Noir Cam') ?></title>
-    <link rel="manifest" href="<?= base_url('manifest.json') ?>">
     <meta name="theme-color" content="#050509">
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
 </head>
@@ -14,7 +13,9 @@
     </div>
     <script>
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('<?= base_url('service-worker.js') ?>').catch(console.error);
+        navigator.serviceWorker.getRegistrations()
+            .then((registrations) => registrations.forEach((registration) => registration.unregister()))
+            .catch(console.error);
     }
     </script>
 </body>
